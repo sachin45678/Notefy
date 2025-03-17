efebe6c538ca475bf4a786ecceaaf9629a1f1601
Sachin Kumar Sharma <sachinkumarsharma4567@gmail.com>
2025-03-17 23:22:11 +0530

    Hashed-password

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,11 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const bcrypt=require("bcrypt")
+
 const userSchema = new Schema({
   fullName: { type: String },
   email: { type: String },
   password: { type: String },
   createdOn: { type: Date, default: new Date().getTime() },
 });
+
+
+
+userSchema.pre('save',async function(next){
+  const salt=await bcrypt.genSalt()
+  this.password=await bcrypt.hash(this.password,salt)
+  next()
+})
 
 module.exports = mongoose.model("User", userSchema);
