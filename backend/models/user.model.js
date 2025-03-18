f86a61d7b21916d41c4f81ac49f7e3f0103b71b8
Sachin Kumar Sharma <sachinkumarsharma4567@gmail.com>
2025-03-18 18:16:59 +0530

    Email-validations

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,21 +1,52 @@
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const bcrypt=require("bcrypt")
+
+// const userSchema = new Schema({
+//   fullName: { type: String },
+//   email: { type: String },
+//   password: { type: String },
+//   createdOn: { type: Date, default: new Date().getTime() },
+// });
+
+
+
+// userSchema.pre('save',async function(next){
+//   const salt=await bcrypt.genSalt()
+//   this.password=await bcrypt.hash(this.password,salt)
+//   next()
+// })
+
+// module.exports = mongoose.model("User", userSchema);
+
+
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
-const bcrypt=require("bcrypt")
+const bcrypt = require("bcrypt");
+const validator = require("validator");  // <-- Import validator
 
 const userSchema = new Schema({
-  fullName: { type: String },
-  email: { type: String },
-  password: { type: String },
-  createdOn: { type: Date, default: new Date().getTime() },
+  fullName: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,   // <-- Email validation
+      message: "Please enter a valid email",
+    },
+  },
+  password: { type: String, required: true},
+  createdOn: { type: Date, default: () => new Date().getTime() },
 });
 
-
-
-userSchema.pre('save',async function(next){
-  const salt=await bcrypt.genSalt()
-  this.password=await bcrypt.hash(this.password,salt)
-  next()
-})
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 module.exports = mongoose.model("User", userSchema);
